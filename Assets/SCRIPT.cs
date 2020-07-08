b96bab1b683760b425617af09d75cc87c0b2ee9d
Kenndly <65460232+Kenndly@users.noreply.github.com>
2020-07-08 21:14:55 +0500

    Тест

---
+++ Assets/SCRIPT.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCRIPT : MonoBehaviour
+{
+    public int GG ;
+   public GameObject GameObject,Canvas;
+    // Start is called before the first frame update   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Score")
+        {
+            GG++;
+            print(GG);
+        }
+        if(other.tag=="Enemy")
+        {   print("Ты клоун");
+            Canvas.SetActive(true);
+            GameObject.SetActive(false);
+            
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
